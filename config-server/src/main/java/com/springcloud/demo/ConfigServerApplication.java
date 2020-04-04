ca39dab85120dfd70bb75432bf346725e7f36fa5
GXiaoFeng <562721575@qq.com>
2020-04-04 14:30:28 +0800

    项目提交

---
+++ config-server/src/main/java/com/springcloud/demo/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer //开启配置中心
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
